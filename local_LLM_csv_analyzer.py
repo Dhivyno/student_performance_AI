0c6a2a98eb0e1a756d6415418c6c007d7badc3ce
Dhivyesh K <63943490+Dhivyno@users.noreply.github.com>
2024-01-30 12:07:07 +0800

    Update local_LLM_csv_analyzer.py

--- local_LLM_csv_analyzer.py
+++ local_LLM_csv_analyzer.py
@@ -6,7 +6,7 @@ pd.set_option('display.max_columns', 500)
 
 df = pd.read_csv('testing.csv')
 
-creds = "Dhivyesh"
+creds = "Zelda Wall"
 
 hash = sha1(creds.encode("utf-8")).hexdigest()
 
@@ -17,7 +17,7 @@ row = df.iloc[value.item()]
 print(row)
 
 name = row["Name"]
-year = int(row["Year"])
+# year = int(row["Year"])
 math = row["Math Grade"]
 physics = row["Physics Grade"]
 chemistry = row["Chemistry Grade"]
@@ -48,21 +48,23 @@ llm_config_assistant={
     "timeout": 600,
     "seed": 42,
     "config_list": config_list_assistant,
-    "temperature": 0
+    "temperature": 0,
+    "stream": True
 }
 
 llm_config_user_proxy={
     "timeout": 600,
     "seed": 42,
     "config_list": config_list_user_proxy,
-    "temperature": 0
+    "temperature": 0,
+    "stream": True
 }
 
 assistant = AssistantAgent(
     name="Assistant",
     llm_config=llm_config_assistant,
-    system_message="Hello, today you are a virtual assistant talking to a parent of a child attending an international school. Your job is to provide information about the child according to the query and the data given to you.",
-    code_execution_config={"work_dir": "web"}
+    system_message="Hello, today you are a virtual assistant talking to a parent of a child attending Garden International School. Your job is to provide information about the child according to the query and the data given to you.",
+    code_execution_config={"work_dir": "web", "use_docker": False},
 )
 
 user_proxy = UserProxyAgent(
@@ -70,7 +72,7 @@ user_proxy = UserProxyAgent(
     human_input_mode="TERMINATE",
     max_consecutive_auto_reply=10,
     is_termination_msg=lambda x: x.get("content", "").rstrip().endswith("TERMINATE"),
-    code_execution_config={"work_dir": "web"},
+    code_execution_config={"work_dir": "web", "use_docker": False},
     llm_config=llm_config_user_proxy,
     system_message="""Reply TERMINATE if the task has been solved at full satisfaction.
 Otherwise, reply CONTINUE, or the reason why the task is not solved yet."""
@@ -78,7 +80,7 @@ Otherwise, reply CONTINUE, or the reason why the task is not solved yet."""
 
 task1 = f"""
 The student's name is {name}.
-The student is in Year {year}.
+
 Math grade is {math}.
 Physics grade is {physics}.
 Chemistry grade is {chemistry}.
@@ -87,13 +89,13 @@ Art grade is {art}.
 Drama grade is {drama}.
 Business grade is {business}.
 Design Technology grade is {dt}.
-An A* grade is excellent.
-An A grade is very good.
-A B grade is good.
-A C grade is a pass.
-A nan grade means the student does not study that subject.
-Any grade other than A*, A, B, C is a fail and unsatisfactory.
-Summarise the performance of the student based on their subject grades like you are talking to the student's parent and explaining their child's academic performance. Don't mention any subjects that the student does not study. Summarise the student's reports and suggest a few improvements the student could make in their academic efforts.
+A* grade is excellent.
+A grade is very good.
+B grade is good.
+C grade is a pass.
+nan grade means the student does not study that subject and you should not discuss it.
+D, E or an F is a fail and unsatisfactory.
+Summarise the performance of the student based on their subject grades like you are talking to the student's parent and explaining their child's academic performance. Say the grades one by one for each subject and explain the meaning of each grade. Don't mention any subjects that the student does not study. Summarise the student's reports and suggest a few improvements the student could make in their academic efforts.
 
 """
 
